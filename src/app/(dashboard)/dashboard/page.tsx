32c31f6c646e6c9e5c4dd9b149947f56dafce72d
Probir Ghosh <probirghosh@outlook.com>
2025-10-22 13:49:27 +0600

    :sparkles: Implement Google Authentication

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,26 @@
-const DashboardHomePage = () => {
+import { getUserSession } from "@/helpers/getUserSession";
+
+const DashboardHomePage = async () => {
+  // const userName = "Probir Ghosh";
+  const quote = "The secret of getting ahead is getting started. – Mark Twain";
+
+  // const session = await getServerSession(authOptions);
+  const session = await getUserSession();
+  console.log(session);
+
   return (
-    <div>
-      <h1>Dashboard Home Page</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6 w-full">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome, {session?.user?.name ? session.user.name : "Guest"}!
+      </h1>
+
+      <p className="text-lg text-gray-600 italic text-center font-bold">
+        {session?.user?.email
+          ? session.user.email
+          : "Please log in to see your email"}
+      </p>
+
+      <p className="text-lg text-gray-600 italic text-center">{quote}</p>
     </div>
   );
 };
---
+++ src/app/(public)/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/modules/Auth/RegisterForm";
+
+const RegisterPage = () => {
+    return (
+        <div className='pt-12'>
+            <RegisterForm/>
+        </div>
+    );
+};
+
+export default RegisterPage;
