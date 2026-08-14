b057b99148122f5c436ddfa1d9fcae8bcffd86c8
Probir Ghosh <probirghosh@outlook.com>
2026-08-14 21:23:58 +0600

    :tada: navbar added

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function ContactLayout({ children }: LayoutProps<"/contact">) {
+  return (
+    <div>
+      <h1>This is contact layout</h1>
+      {children}
+    </div>
+  );
+}   
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/layout/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,10 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="en" className={cn("font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
